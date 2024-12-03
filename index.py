627c157a5639515ea4b9dec59a0d2e082c6034e1
nicolashiguera17 <189983438+nicolashiguera17@users.noreply.github.com>
2024-12-03 08:04:20 -0500

    grego el index.py a mi repo

---
+++ index.py
@@ -0,0 +1,28 @@
+#VARIABLES
+#TIPOS DE DATOS PRIMITIVOS
+#-string = cadenas de caracteres 'str'
+#-char = caracter 'str'
+# - number = enteros 'int' , decimal 'float'
+#- bolean - datos logicos 'bool'
+name = "nicolas diaz higuera"
+age = 17 
+height = 1.70
+trainer = False
+
+# CONJUNTO DE DATOS
+# -array = conjuntos de datos de una dimension 
+hobbie = list()
+hobbie = list["voleibol", "cantar", "dormir"]
+
+
+# MODIFICAR UN DATO DE UNA LISTA 
+#hobbies[2] = despertarme
+
+#SOLICITAR DATOS EN LA CONSOLA Y GUARDARLOS EN LA LISTA 
+#dato = input("ingrese otro pasatiempo: ")
+#hobbies.append (dato)
+#print (dato)
+
+#ELIMINAR EL ULTIMO DATO DE LA LISTA 
+hobbie.pop()
+print (hobbie)
